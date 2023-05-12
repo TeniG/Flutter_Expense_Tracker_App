05d288f183d982821f55a15b6e8b2b85ad8b48d1
TeniG <tenigada@gmail.com>
2023-05-12 20:43:47 +0530

    - Theme and Color scheme implemented.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_expense_tracker_app/widgets/expenses_screen.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+              backgroundColor: kColorScheme.onPrimaryContainer,
+              foregroundColor: kColorScheme.onPrimary),
+          cardTheme: const CardTheme().copyWith(
+              margin: const EdgeInsets.symmetric(
+            vertical: 6,
+            horizontal: 16,
+          )),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.onPrimary,
+            ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                  
+                  fontWeight: FontWeight.normal,
+                  color: kColorScheme.onSecondaryContainer
+                ),
+              )),
+      home: const Expenses(),
     ),
   );
 }
